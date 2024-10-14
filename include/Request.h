d9766e13871075b85b8f10dde9843775a95722a0
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-10-14 18:56:38 +0800

    refactor: Rewrite the classes for encapsulation

--- include/Request.h
+++ include/Request.h
@@ -1,33 +1,7 @@
 #pragma once
-#ifndef REQUEST_H_
-#define REQUEST_H_
+#ifndef _REQUEST_H_
+#define _REQUEST_H_
 
-#include <unordered_map>
-#include <string>
-#include <string_view>
 
-class Request {
-private:
-  std::string raw_;
-  std::string_view method_;
-  std::string_view uri_;
-  std::string_view version_;
-  std::unordered_map<std::string_view, std::string_view> headers_;
-  std::string_view body_;
 
-  const void parse_request_line();
-
-public:
-  explicit Request();
-  explicit Request(const std::string &raw);
-
-  virtual ~Request() = default;
-
-  std::string_view method() const;
-
-  const std::string_view request_line() const;
-  const std::string_view headers() const;
-  const std::string_view body() const;
-};
-
-#endif // REQUEST_H_
+#endif
