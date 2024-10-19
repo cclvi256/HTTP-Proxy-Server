dba3ddb32030b3659215c3c82203896b28309294
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-10-19 14:59:09 +0800

    refactor: Use common util to simplify code

--- src/Request.cc
+++ src/Request.cc
@@ -1,4 +1,5 @@
 #include "Request.h"
+#include "util.h"
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -38,35 +39,6 @@ RequestLine parse_request_line(std::string_view raw) {
       std::string(raw.substr(version_start, version_end - version_start));
 
   return {method, uri, version};
-}
-
-std::unordered_map<std::string, std::string>
-parse_headers(std::string_view raw) {
-  std::unordered_map<std::string, std::string> headers;
-  auto total_length = raw.size();
-  auto linebreak = raw.find("\r\n");
-
-  auto start = linebreak + 2;
-  while (start < total_length) {
-    auto colon = raw.find(':', start);
-    if (colon == std::string::npos) {
-      break;
-    }
-
-    auto key_start = start;
-    auto key_end = colon;
-    auto value_start = colon + 2;
-    auto value_end = raw.find("\r\n", value_start);
-
-    std::string key = std::string(raw.substr(key_start, key_end - key_start));
-    std::string value =
-        std::string(raw.substr(value_start, value_end - value_start));
-
-    headers[key] = value;
-    start = value_end + 2;
-  }
-
-  return headers;
 }
 
 Request::Request(const std::string &method, const std::string &uri,
--- src/Response.cc
+++ src/Response.cc
@@ -1,4 +1,5 @@
 #include "Response.h"
+#include "util.h"
 #include <stdexcept>
 
 struct StatusLine {
@@ -37,35 +38,6 @@ StatusLine parse_status_line(std::string_view raw) {
       std::string(raw.substr(message_start, message_end - message_start));
 
   return {version, status, message};
-}
-
-std::unordered_map<std::string, std::string>
-parse_headers(std::string_view raw) {
-  std::unordered_map<std::string, std::string> headers;
-  auto total_length = raw.size();
-  auto linebreak = raw.find("\r\n");
-
-  auto start = linebreak + 2;
-  while (start < total_length) {
-    auto colon = raw.find(':', start);
-    if (colon == std::string::npos) {
-      break;
-    }
-
-    auto key_start = start;
-    auto key_end = colon;
-    auto value_start = colon + 2;
-    auto value_end = raw.find("\r\n", value_start);
-
-    std::string key = std::string(raw.substr(key_start, key_end - key_start));
-    std::string value =
-        std::string(raw.substr(value_start, value_end - value_start));
-
-    headers[key] = value;
-    start = value_end + 2;
-  }
-
-  return headers;
 }
 
 Response::Response(const std::string &version, const std::string &status,
