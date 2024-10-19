c24e2ffce14c63a8327177d0480b9f2fdcc4b90a
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-10-19 14:13:54 +0800

    feat: Add request defination

--- include/Request.h
+++ include/Request.h
@@ -1,8 +1,29 @@
 #pragma once
 
+#include <string>
+#include <string_view>
+#include <unordered_map>
 class Request {
-  private:
+private:
+  std::string method_;
+  std::string uri_;
+  std::string version_;
+  std::unordered_map<std::string, std::string> headers_;
+  std::string body_;
 
-  public:
+public:
+  Request() = default;
+  Request(const std::string &method, const std::string &uri,
+          const std::string &version,
+          const std::unordered_map<std::string, std::string> &headers,
+          const std::string &body);
+  Request(const std::string_view &request);
+  Request(const Request &request);
+  Request(Request &&request);
 
+  const std::string method() const;
+  const std::string uri() const;
+  const std::string version() const;
+  const std::unordered_map<std::string, std::string> headers() const;
+  const std::string header_query(const std::string &key) const;
 };
